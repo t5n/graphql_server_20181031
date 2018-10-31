f9fe09e623ce29f25fdb97e556cf580c685e6a77
Tony Jin <tonyjin@tjsbuinormatics.uhmc.sbuh.stonybrook.edu>
2018-10-31 11:31:35 -0400

    index.js bracket typo

--- index.js
+++ index.js
@@ -50,8 +50,14 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
-//server.listen().then(({ url }) => {
+// server.listen().then(({ url }) => {
 //  console.log(`🚀  Server ready at ${url}`);
+//  });
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+server.listen(process.env.PORT || 4000).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`)
+ });
+
+
+
+
