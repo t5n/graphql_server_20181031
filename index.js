15f35b1a5f56727f9a9cf68c5d91f06bbc267680
Tony Jin <tonyjin@tjsbuinormatics.uhmc.sbuh.stonybrook.edu>
2018-10-31 11:03:08 -0400

    Configuring Variable Port

--- index.js
+++ index.js
@@ -50,6 +50,10 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
-server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+//server.listen().then(({ url }) => {
+//  console.log(`🚀  Server ready at ${url}`);
+
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
 });
