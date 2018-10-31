b698a9df979239dab3eb3a362d4ffdb105c206e0
Tony Jin <tonyjin@tjsbuinormatics.uhmc.sbuh.stonybrook.edu>
2018-10-31 11:16:31 -0400

    fixed bracket typo

--- index.js
+++ index.js
@@ -56,4 +56,3 @@ const server = new ApolloServer({ typeDefs, resolvers });
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`);
 });
-});
